58b3b062cad7b041b0d3a5510d1225a00d3d17ba
Goutham Kumar Y <gouthamproai@gmail.com>
2019-10-23 18:23:36 +0530

    Final draft for sftppull 23 oct 2019

--- sftpget.py
+++ sftpget.py
@@ -1,14 +1,18 @@
 import pysftp
-import boto3
+#import boto3
 
-r_filepath='<path to directory of remote file>'
-r_filename='<name of remote file>'
+r_filepath='/quarantine-file-store'
+r_filename='test2.txt'
 l_file=r_filename
 s3_bucket_name='<name of s3 bucket>'
-s3=boto3.client('s3')
+#s3=boto3.client('s3')
 
-def handler(event,context):
-	with pysftp.Connection('hostname', username='me', password='secret',port='') as sftp:
+def handler():
+	cnopts = pysftp.CnOpts()
+	cnopts.hostkeys = None
+	with pysftp.Connection('s-66dd554c71f047eea.server.transfer.us-east-1.amazonaws.com', username='testuser', private_key='sftpuser',cnopts=cnopts) as sftp:
 		with sftp.cd(r_filepath): 
 			sftp.get(r_filename,localpath=l_file)
-	s3.upload(l_file,s3_bucket_name)
+	#s3.upload(l_file,s3_bucket_name)
+
+handler()
